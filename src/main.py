0a36a7e188513af5d7a212fbc602a5a23d3c03fb
Faysal Ahmed <faysal15-1655@diu.edu.bd>
2024-10-30 17:47:41 +0600

    feat:delete functionalities add

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI,HTTPException,Depends
 import uvicorn 
-from database.database import read_json_file,write_json_file
 import models.userModels
-from schemas.users import UserCreate,UserUpdate
+from sqlalchemy import text
+from schemas.users import UserOut,UserUpdate,UserIn
 from database.databaseSQL import engine,SessionLocal
 from sqlalchemy.orm import Session
 import models
+from typing import List
 app = FastAPI()
 
 models.userModels.Base.metadata.create_all(bind=engine)
@@ -17,21 +18,16 @@ def get_db():
     finally:
         db.close()    
 
-@app.get("/users")
+@app.get("/users", response_model=List[UserOut])
 def user_list(db: Session = Depends(get_db)):
-    userlist = db.query(models.userModels.Users).all()
-    return {"data":userlist}
+    data = db.query(models.userModels.Users).all()
+    return data
 
-@app.get("/users/{id}")
-def user(id:int,db: Session = Depends(get_db)):
-    userModel = db.query(models.userModels.Users).filter(models.userModels.Users.id==id).first()
-    if userModel is None:
-        raise HTTPException(status_code=404, detail="User with given id doesn't exists") 
-    return [{"data":userModel}]      
-    
-
-@app.post("/users/")
-def create_user(user:UserCreate,db: Session = Depends(get_db)):
+@app.post("/users/",response_model=UserOut)
+def create_user(user:UserIn,db: Session = Depends(get_db)):
+    userCheck =db.query(models.userModels.Users).filter(models.userModels.Users.id==user.id).first()
+    if userCheck is not None:
+        raise HTTPException(status_code=403,detail="User with same id already exists")
     userModel = models.userModels.Users()
     userModel.id=user.id
     userModel.name = user.name
@@ -40,33 +36,40 @@ def create_user(user:UserCreate,db: Session = Depends(get_db)):
     db.add(userModel)
     db.commit()
     db.refresh(userModel)
-    return [{"data":userModel}]
+    return userModel
 
 
-@app.put("/users/{id}")
+@app.get("/users/{id}",response_model=UserOut)
+def user(id:int,db: Session = Depends(get_db)):
+    userModel = db.query(models.userModels.Users).filter(models.userModels.Users.id==id).first()
+    if userModel is None:
+        raise HTTPException(status_code=404, detail="User with given id doesn't exists") 
+    return userModel     
+    
+
+@app.put("/users/{id}",response_model=UserOut)
 def update_user(id: int, data: UserUpdate,db: Session = Depends(get_db)):
     userModel = db.query(models.userModels.Users).filter(models.userModels.Users.id==id).first()
-    receivedData=data.dict(exclude_unset=True)
+
+    if userModel is None:
+        raise HTTPException(status_code=404, detail="Data with the given id not found.") 
     for key,value in data.dict(exclude_unset=True).items():
         setattr(userModel,key,value)
     db.commit()
     db.refresh(userModel)    
     return userModel
-
-    if userModel is None:
-        raise HTTPException(status_code=404, detail="Data with the given id not found.")    
+    
 
 @app.delete("/users/{id}")
-def delete_user(id:int):
-    userlist = read_json_file()
-    deletedUser={}
-    for index,user in enumerate(userlist):
-        if user["id"]== id:
-            deletedUser=userlist[index]
-            del userlist[index]
-            write_json_file(userlist)
-            return {"message":deletedUser}
-    raise HTTPException(status_code=404,detail="User with the given id not found")    
+def delete_user(id:int,db: Session = Depends(get_db)):
+    user = db.query(models.userModels.Users).filter(models.userModels.Users.id==id).first()
+    if user is None:
+        raise HTTPException(status_code=404,detail="User with the given id not found")    
+    db.delete(user)
+    db.commit()
+    return {"message":"Data deleted"}
+
+
 
 if __name__ == "__main__":
     uvicorn.run(
